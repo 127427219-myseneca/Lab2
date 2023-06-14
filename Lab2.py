5f04f9367396deb5915a80abd880d7c7c4ef4196
vikas-singh-06 <06vikaschauhan@gmail.com>
2023-06-14 18:05:10 -0400

    Updated Hello World finction

--- Lab2.py
+++ Lab2.py
@@ -1,13 +1,4 @@
-import sys
+def helloWorld():
+    print('Hello World')
 
-def print_arguments():
-    script_name = sys.argv[0]
-    arguments = sys.argv[1:]
-
-    print("Script Name:", script_name)
-    print("Arguments:", arguments)
-
-    # Print script name and arguments combined
-    print("Script and Arguments:", script_name, *arguments)
-
-print_arguments()
+helloWorld()
